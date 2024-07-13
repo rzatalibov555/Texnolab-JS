c000950db461c379e30dcdd17a4a0d40b32eff4f
rzatalibov555 <sethubpro@gmail.com>
2024-07-13 18:20:38 +0400

    JS lesson 1 Outputs

---
+++ js/index.js
@@ -0,0 +1,52 @@
+
+// document.write("Lesson JS")
+// document.write("<br> How are you?")
+/*window.alert("This is alert window") */
+// console.log("test")
+
+
+// document.getElementById('test1').innerHTML = ""
+
+
+// document.getElementsByClassName('test')[0].innerHTML = "Super"
+// document.getElementsByClassName('test')[2].innerHTML = "Super"
+
+// document.getElementsByTagName('P')[0].innerHTML = "Hello"
+// document.getElementsByTagName('P')[1].innerHTML = "Hello"
+// document.getElementsByTagName('P')[2].innerHTML = "Hello"
+
+// document.getElementsByTagName('p')[0].innerHTML = "<p style='color:red'>Hello 1</p>"
+// document.getElementsByTagName('p')[1].innerText = "<p style='color:red'>Hello 2</p>"
+
+// print()
+
+// function changeToDarkMode() {
+//     let a = confirm("Are you sure want delete this item?")
+
+//     if (a) {
+//         document.body.style.background = "black"
+//         document.body.style.color = "white"
+//     } else {
+//         alert("Istemediniz. Ozunuz bilersiz ;D")
+//     }
+// }
+
+// alert("Xos geldiniz")
+// confirm("Melumatlarin yoxlanilmasina icaze verirsinizmi?")
+
+
+// function demo() {
+//     let a = prompt("Yawinizi qeyd edin")
+
+//     if (a == 'Rza') {
+//         document.body.style.background = "Green"
+//     } else if (a == 'Fizza') {
+//         document.body.style.background = "Pink"
+//     } else {
+//         alert("Sehv")
+//         document.body.style.background = "white"
+//         // window.location.href = "";
+//         window.open("http://www.w3schools.com", "_blank")
+//     }
+// }
+
---
+++ js/main.js
@@ -0,0 +1,74 @@
+document.getElementsByTagName('h1')[0].innerHTML="Fraz" 
+
+// document.getElementsByTagName('p')[1].innerHTML="Fizza"
+
+// document.getElementsByTagName('h1')[2].innerHTML="Eshban"
+
+
+// var
+// let 
+// const
+
+
+// Data Types
+// let a = "Rza" // String // Str // Text
+// let b = 6     // Number // Num // Integer // Int
+// let c = true  //Boolean // Bool // true = 1, false = 0
+
+// let d 
+// let e = null
+
+// let t = new Date()
+// console.log(t)
+
+// d = "salam"
+// console.log(d)
+// console.log("Rza "+"Talibov")
+// console.log(10+11)
+
+
+let FirstName = "Mammadrza"
+let Surname = "Talibov"
+let bDay = 1991
+let current_year = 2024
+let Company = "Facebbok"
+// proqramming language
+// car
+// School
+// Country
+// hobby
+// pet
+// work
+// gender
+// Movie
+// from
+// game
+
+
+// console.log("My name is "+FirstName+" "+Surname+". I am "+(current_year-bDay)+" years old.")
+// console.log(`My name is ${FirstName} ${Surname}. I am ${current_year-bDay} years old.`)    
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let ad = "Rza"
+// let Ad = "Eshban"
+// let aD = "Fraz"
+// let _aD = "Fizza"
+// let $aD = "Zara"
+
+// console.log(ad)
+// console.log(Ad)
+// console.log(aD)
+// console.log(_aD)
+// console.log($aD)
+
