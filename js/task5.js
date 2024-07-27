a98591c83ae70dde063cec0a40365d47944108ca
rzatalibov555 <sethubpro@gmail.com>
2024-07-27 18:09:42 +0400

    lesson 19

--- js/task5.js
+++ js/task5.js
@@ -1,73 +1,175 @@
 
-var top_side = 0
-var left_side = 0
+// var top_side = 0
+// var left_side = 0
 
-var top_side2 = 0
-var left_side2 = 0
+// var top_side2 = 0
+// var left_side2 = 0
 
-var z_index = 0
+// var z_index = 0
 
 
 
-onkeydown = (e) =>{
+// onkeydown = (e) =>{
 
-    var player1 = document.getElementById('player1')
-    var player2 = document.getElementById('player2')
+//     var player1 = document.getElementById('player1')
+//     var player2 = document.getElementById('player2')
 
-    if(e.key === "o"){
-       player1.style.zIndex = z_index
-       z_index++
-    }
+//     if(e.key === "o"){
+//        player1.style.zIndex = z_index
+//        z_index++
+//     }
     
-    if(e.key === "p"){
-        player2.style.zIndex = z_index
-        z_index++
-    }
+//     if(e.key === "p"){
+//         player2.style.zIndex = z_index
+//         z_index++
+//     }
     
 
 
-    if(e.key === "ArrowUp"){
-        player1.style.top = top_side + "px"
-        top_side -= 10
-    }
+//     if(e.key === "ArrowUp"){
+//         player1.style.top = top_side + "px"
+//         top_side -= 10
+//     }
 
-    if(e.key === "ArrowDown"){
-        player1.style.top = top_side + "px"
-        top_side += 10
-    }
+//     if(e.key === "ArrowDown"){
+//         player1.style.top = top_side + "px"
+//         top_side += 10
+//     }
 
-    if(e.key === "ArrowLeft"){
-        player1.style.left = left_side + "px"
-        left_side -= 10
-    }
+//     if(e.key === "ArrowLeft"){
+//         player1.style.left = left_side + "px"
+//         left_side -= 10
+//     }
 
-    if(e.key === "ArrowRight"){
-        player1.style.left = left_side + "px"
-        left_side += 10
-    }
-
-
+//     if(e.key === "ArrowRight"){
+//         player1.style.left = left_side + "px"
+//         left_side += 10
+//     }
 
 
-    if(e.key === "w"){
-        player2.style.top = top_side2 + "px"
-        top_side2 -= 10
-    }
 
-    if(e.key === "s"){
-        player2.style.top = top_side2 + "px"
-        top_side2 += 10
-    }
 
-    if(e.key === "a"){
-        player2.style.left = left_side2 + "px"
-        left_side2 -= 10
-    }
+//     if(e.key === "w"){
+//         player2.style.top = top_side2 + "px"
+//         top_side2 -= 10
+//     }
 
-    if(e.key === "d"){
-        player2.style.left = left_side2 + "px"
-        left_side2 += 10
-    }
+//     if(e.key === "s"){
+//         player2.style.top = top_side2 + "px"
+//         top_side2 += 10
+//     }
+
+//     if(e.key === "a"){
+//         player2.style.left = left_side2 + "px"
+//         left_side2 -= 10
+//     }
+
+//     if(e.key === "d"){
+//         player2.style.left = left_side2 + "px"
+//         left_side2 += 10
+//     }
 
     
+// }
+
+
+
+
+
+// = 
+// let a = 5
+// console.log(a)
+
+// ==	equal to
+// !=	not equal
+
+
+
+// ===	equal value and equal type
+// !==	not equal value or not equal type
+
+//     >	greater than
+//     <	less than
+
+//     >=	greater than or equal to
+//     <=	less than or equal to
+
+//     ?	ternary operator  (if)
+//     :    ternary operator  (else)
+
+// var demo = 4111
+
+// demo == 5 ? console.log('yes') : demo == 4 ? console.log("this is 4") : console.log("nea")
+
+// if(demo == 5){
+//     console.log('yes')
+// }else if(demo == 4){
+//     console.log("this is 4")
+// }else{
+//     console.log('no')
+// }
+
+
+
+
+// function add_num(){
+//     let demo = document.getElementById('demo').value
+
+//     if(18 <= demo){
+//         console.log("Welcome")
+//     }else{
+//         console.log("Go to school")
+//     }
+// }
+
+
+// let text1 = "What a very ";
+// text1 += "nice day";
+
+
+// let text1 = "What a very "
+// // text1 += "nice day"
+
+// text1+="nice day"
+
+
+
+// var left_side = 5
+
+
+
+
+// left_side += 10
+
+// console.log(left_side)
+
+
+
+function login(){
+    let username = document.getElementById("username").value
+    let password = document.getElementById("password").value
+    let secret = document.getElementById("secret").value
+    
+    // let username_db = "admin"
+    // let password_db = "123"
+    // let secret_code = "car"
+
+    // if(username_db == username && password_db == password || secret_code == secret){
+    //     console.log("welcome")
+    // }else{
+    //     console.log('Username or password is wrong')
+    // }
+
+
+    // if(!username){
+    //     console.log("yes")
+    // }else{
+    //     console.log("no")
+    // }
 }
+
+
+
+
+
+
